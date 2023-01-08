ac146a6e68692885e53466fb5ac581605301e3f7
LoubatonAv <aegtar88@gmail.com>
2023-01-08 19:02:28 +0200

    4TH

--- src/service/httpService.js
+++ src/service/httpService.js
@@ -1,8 +1,7 @@
 import Axios from 'axios';
 
+const BASE_URL = process.env.NODE_ENV === 'production' ? '//whistle-tabs-api.onrender.com/' : '//localhost:3030/api/';
 // const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
-const BASE_URL =
-  process.env.NODE_ENV === 'production' ? 'https://whistle-tabs-api.onrender.com' : '//localhost:3030/api/';
 
 var axios = Axios.create({
   withCredentials: true,
